46fa212e4ca32321f57109e84d5e9e4dfee1ff8b
jl9404 <jl9404@hotmail.com>
2016-02-26 02:52:08 +0800

    delete getroomlist

--- Channl32/UserList.h
+++ Channl32/UserList.h
@@ -255,28 +255,6 @@ public:
 					for (int i = 0; i < 13; i++)LRR->zeros[i] = 0;
 			}
 	}
-	void GetRoomList(RoomListResponse* RLR) {
-		int x = 0;
-		for (int i = 0; i < MaxRoom; i++)
-			if (Rooms[i].n != -1) {
-				RLR->roomnumber[x] = Rooms[i].n;
-				strcpy(RLR->title[x], Rooms[i].title);
-				RLR->mode[x] = Rooms[i].mode;
-				RLR->map[x] = Rooms[i].map;
-				RLR->maxplayers[x] = Rooms[i].maxp;
-				RLR->unks2[x] = 1;
-				RLR->unks4[x] = -1;
-				for (int j = 0; j < Rooms[i].maxp - 1; j++) {
-					RLR->players[x][j] = Rooms[i].Player[j] ? Rooms[i].Player[j]->Info.usr_char : 0;
-				}
-				x++;
-			}
-		while (x < MaxRoom)
-		{
-			RLR->roomnumber[x] = -1;
-			x++;
-		}
-	}
 	void GetRoomPlayerList(int n, RoomPlayerListResponse *RPLR) {
 		for (int i = 0; i < MaxRoom; i++)
 			if (Rooms[i].n == n) {
