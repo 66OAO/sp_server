66OAO/sp_server

commit a0c27f7a517c2133fc327783f5e642cafe0b693a

1 : move required dll from lib to bin folder automatically after build channel or login server 2 : remove packet redundancy in exit response 3 : detect user id when disconnecting with login server

Login32/LoginServer.cpp
@@ -56,13 +56,14 @@ bool cLoginServer::Start()
 	return true;
 }
 
-void Comm(void *msg_sock)
+void Comm(void *param)
 {
-	SOCKET msg_socket = (SOCKET)msg_sock;
+	socketWithUID *args = (socketWithUID*)param;
+	SOCKET msg_socket = (SOCKET)args->msg_socket;
+	int usr_id = args->usr_id;
 	unsigned char buffer[2000];
 	int retbufsize = 0;
-
-	while (msg_sock)
+	while (msg_socket)
 	{
 		retbufsize = recv(msg_socket, (char*)buffer, sizeof(buffer), 0);
 
@@ -70,6 +71,9 @@ void Comm(void *msg_sock)
 		{
 			Log::Info("Connection closed by client");
 			closesocket(msg_socket);
+			if (usr_id != -1) {
+				Log::Info("User ID : {} Disconnect", usr_id);
+			}
 			break;
 		}
 
@@ -77,6 +81,9 @@ void Comm(void *msg_sock)
 		{
 			Log::Info("Client socket closed");
 			closesocket(msg_socket);
+			if (usr_id != -1) {
+				Log::Info("User ID : {} Disconnect", usr_id);
+			}
 			break;
 		}
 		else Log::Out("recv {} bytes", retbufsize);
@@ -89,12 +96,14 @@ void Comm(void *msg_sock)
 
 		//outBuffer();
 		try {
-			PacketHandler PackHandle(buffer);
-			if(PackHandle.nOfPackets) {
+			PacketHandler PackHandle(buffer, usr_id);
+			if (PackHandle.nOfPackets) {
 				Log::Info("Sending Response");
 				retbufsize = send(msg_socket, (char*)buffer, PackHandle.ServerResponse(buffer)*buffer[0], 0);
-			} else Log::Warning("Server has no response");
-		} catch (const std::exception & e) {
+			}
+			else Log::Warning("Server has no response");
+		}
+		catch (const std::exception & e) {
 			Log::Error("Error handling client packet: {}", e.what());
 		}
 
@@ -106,7 +115,6 @@ bool cLoginServer::CommLoop()
 {
 	bool bExit = false;
 	int retbufsize = 0;
-
 	while (!bExit)
 	{
 		if ((msg_socket = accept(listen_socket, (struct sockaddr*)&client, &clientlen)) == INVALID_SOCKET)
@@ -116,7 +124,12 @@ bool cLoginServer::CommLoop()
 		}
 		else
 			Log::Out("Accept Client: {}", inet_ntoa(client.sin_addr));
-		_beginthread(Comm, 0, (void *)msg_socket);
+		int id = -1;
+		socketWithUID *arg;
+		arg = (socketWithUID *)malloc(sizeof(socketWithUID));
+		arg->msg_socket = (void*)msg_socket;
+		arg->usr_id = id;
+		_beginthread(Comm, 0, (void*)arg);
 	}
 
 	return true;

Login32/LoginServer.h
@@ -21,5 +21,9 @@ private:
 	SOCKET listen_socket, msg_socket;
 };
 
+typedef struct {
+	void* msg_socket;
+	int usr_id;
+}socketWithUID;
 
 #endif

.gitignore
@@ -13,7 +13,6 @@
 # Compiled Dynamic libraries
 *.so
 *.dylib
-*.dll
 
 # Fortran module files
 *.mod

Channl32/Channl32_2015.vcxproj
@@ -54,9 +54,11 @@
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <ReferencePath>$(ReferencePath)</ReferencePath>
+    <CustomBuildAfterTargets>Build</CustomBuildAfterTargets>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
     <ReferencePath>$(ReferencePath)</ReferencePath>
+    <CustomBuildAfterTargets>Build</CustomBuildAfterTargets>
   </PropertyGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <ClCompile>
@@ -80,6 +82,12 @@
       <SubSystem>Console</SubSystem>
       <TargetMachine>MachineX86</TargetMachine>
     </Link>
+    <CustomBuildStep>
+      <Command>robocopy "$(SolutionDir)lib" "$(SolutionDir)bin\$(ProjectName)_$(Configuration)" /NFL /NDL /NJH /NJS /nc /ns /np  || exit /B 0</Command>
+    </CustomBuildStep>
+    <CustomBuildStep>
+      <Outputs>output</Outputs>
+    </CustomBuildStep>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
     <ClCompile>
@@ -102,6 +110,12 @@
       <TargetMachine>MachineX86</TargetMachine>
       <AdditionalDependencies>ws2_32.lib;libmysql.lib;stlx.lib;%(AdditionalDependencies)</AdditionalDependencies>
     </Link>
+    <CustomBuildStep>
+      <Command>robocopy "$(SolutionDir)lib" "$(SolutionDir)bin\$(ProjectName)_$(Configuration)" /NFL /NDL /NJH /NJS /nc /ns /np  || exit /B 0</Command>
+    </CustomBuildStep>
+    <CustomBuildStep>
+      <Outputs>output</Outputs>
+    </CustomBuildStep>
   </ItemDefinitionGroup>
   <ItemGroup>
     <ClCompile Include="..\shared\IOSocket.cpp" />

Channl32/PacketHandler.cpp
@@ -935,9 +935,7 @@ void PacketHandler::GenerateResponse(int ResponsePacketType)
 			{
 				if (Info.usr_team == 10) RoomList.Rooms[i].blueteam--;
 				else if (Info.usr_team == 20) RoomList.Rooms[i].redteam--;
-			}
-			if (RoomList.Rooms[i].n != -1) {
-				HandleList.ProdcastRoomUpdate(RoomList.Rooms[i].n);
+				break;
 			}
 		}
 	}
@@ -950,9 +948,7 @@ void PacketHandler::GenerateResponse(int ResponsePacketType)
 			{
 				if (Info.usr_team == 10) RoomList.Rooms[i].blueteam--;
 				else if (Info.usr_team == 20) RoomList.Rooms[i].redteam--;
-			}
-			if (RoomList.Rooms[i].n != -1) {
-				HandleList.ProdcastRoomUpdate(RoomList.Rooms[i].n);
+				break;
 			}
 		}
 		nOfPackets = 0;

Login32/Login32_2015.vcxproj
@@ -51,6 +51,12 @@
     <LibraryPath Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">..\dep\mysql-connector-c-6.1.6-win32\lib;..\dep\stlx\lib;$(LibraryPath)</LibraryPath>
     <TargetName Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">$(ProjectName)_d</TargetName>
   </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <CustomBuildAfterTargets>Build</CustomBuildAfterTargets>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <CustomBuildAfterTargets>Build</CustomBuildAfterTargets>
+  </PropertyGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <ClCompile>
       <Optimization>Disabled</Optimization>
@@ -72,6 +78,12 @@
       <SubSystem>Console</SubSystem>
       <TargetMachine>MachineX86</TargetMachine>
     </Link>
+    <CustomBuildStep>
+      <Command>robocopy "$(SolutionDir)lib" "$(SolutionDir)bin\$(ProjectName)_$(Configuration)" /NFL /NDL /NJH /NJS /nc /ns /np  || exit /B 0</Command>
+    </CustomBuildStep>
+    <CustomBuildStep>
+      <Outputs>output</Outputs>
+    </CustomBuildStep>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
     <ClCompile>
@@ -94,6 +106,12 @@
       <TargetMachine>MachineX86</TargetMachine>
       <AdditionalDependencies>ws2_32.lib;libmysql.lib;stlx.lib;%(AdditionalDependencies)</AdditionalDependencies>
     </Link>
+    <CustomBuildStep>
+      <Command>robocopy "$(SolutionDir)lib" "$(SolutionDir)bin\$(ProjectName)_$(Configuration)" /NFL /NDL /NJH /NJS /nc /ns /np  || exit /B 0</Command>
+    </CustomBuildStep>
+    <CustomBuildStep>
+      <Outputs>output</Outputs>
+    </CustomBuildStep>
   </ItemDefinitionGroup>
   <ItemGroup>
     <ClCompile Include="..\shared\IOSocket.cpp" />

Login32/PacketHandler.cpp
@@ -6,7 +6,7 @@ extern Ini config;
 IOSocket cIOSocket;
 int PacketHandler::nOfPackets = 0, PacketHandler::State = -1;
 
-PacketHandler::PacketHandler(unsigned char* buf)
+PacketHandler::PacketHandler(unsigned char* buf, int &socket_usr_id)
 {
 	nOfPackets = 0;
 	Decrypt(buf);
@@ -17,6 +17,7 @@ PacketHandler::PacketHandler(unsigned char* buf)
 		Login_Info = (LoginInfo*)buf;
 		Log::Info("Client login attempt: {} // {}", Login_Info->username, Login_Info->password);
 		GenerateResponse(LOGIN_RESPONSE);
+		socket_usr_id = usr_id;
 		break;
 	case SERVERINFO_REQ:
 	case AFTER_SERVERINFO_REQ:

Login32/PacketHandler.h
@@ -26,7 +26,7 @@ private:
 
 
 public:
-	PacketHandler(unsigned char*);
+	PacketHandler(unsigned char*, int&);
 	~PacketHandler();
 	int UpdateState();
 	int IdentifyPacketType();

commit 15065f386250d354ae72ed7558da942c42b2f68f

Refactor; Fixed memory leak introduced by a0c27f7a517c2133fc327783f5e642cafe0b693a

Channl32/main.cpp
@@ -18,17 +18,17 @@ int main()
 	//cout << hex << sizeof(QuestGainResponse) << endl;
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	ChannelServer *CS = new ChannelServer;
-
-	config.SetSection("CHANNELS");
-	u32 port = config.ReadInt("port", 9303);
-
-	if (CS->Start(port))
 	{
-		Log::Out("----- Channel Server Started -----\n");
+		UniquePtr<ChannelServer> channel_server = MakeUnique<ChannelServer>();
+
+		config.SetSection("CHANNELS");
+		u32 port = config.ReadInt("port", 9303);
+
+		if(channel_server->Start(port)) {
+			Log::Out("----- Channel Server Started -----\n");
+		}
+		channel_server->CommLoop();
 	}
-	CS->CommLoop();
-	delete CS;
 
 	cout << endl;
 	if (SAVELOG)

Login32/LoginServer.cpp
@@ -3,7 +3,7 @@
 
 extern Ini config;
 
-cLoginServer::cLoginServer()
+LoginServer::LoginServer()
 {
 	//for (int i = 0; i < BUFFER_SIZE; i++)
 	  //  buffer[i] = 0;
@@ -12,12 +12,13 @@ cLoginServer::cLoginServer()
 	clientlen = sizeof(client);
 }
 
-cLoginServer::~cLoginServer()
+LoginServer::~LoginServer()
 {
+	Log::Out("Server Closing");
 	WSACleanup();
 }
 
-bool cLoginServer::Start()
+bool LoginServer::Start()
 {
 	if (WSAStartup(514, &wsaData))
 	{
@@ -56,21 +57,20 @@ bool cLoginServer::Start()
 	return true;
 }
 
-void Comm(void *param)
+void Comm(void *args)
 {
-	socketWithUID *args = (socketWithUID*)param;
-	SOCKET msg_socket = (SOCKET)args->msg_socket;
-	int usr_id = args->usr_id;
-	unsigned char buffer[2000];
-	int retbufsize = 0;
-	while (msg_socket)
+	SOCKET socket = (SOCKET)args;
+	int usr_id = -1;
+	u8 buffer[2048];
+
+	while (socket)
 	{
-		retbufsize = recv(msg_socket, (char*)buffer, sizeof(buffer), 0);
+		auto retbufsize = recv(socket, (char*)buffer, sizeof(buffer), 0);
 
 		if (!retbufsize)
 		{
 			Log::Info("Connection closed by client");
-			closesocket(msg_socket);
+			closesocket(socket);
 			if (usr_id != -1) {
 				Log::Info("User ID : {} Disconnect", usr_id);
 			}
@@ -80,7 +80,7 @@ void Comm(void *param)
 		if (retbufsize == SOCKET_ERROR)
 		{
 			Log::Info("Client socket closed");
-			closesocket(msg_socket);
+			closesocket(socket);
 			if (usr_id != -1) {
 				Log::Info("User ID : {} Disconnect", usr_id);
 			}
@@ -99,7 +99,7 @@ void Comm(void *param)
 			PacketHandler PackHandle(buffer, usr_id);
 			if (PackHandle.nOfPackets) {
 				Log::Info("Sending Response");
-				retbufsize = send(msg_socket, (char*)buffer, PackHandle.ServerResponse(buffer)*buffer[0], 0);
+				retbufsize = send(socket, (char*)buffer, PackHandle.ServerResponse(buffer)*buffer[0], 0);
 			}
 			else Log::Warning("Server has no response");
 		}
@@ -111,7 +111,7 @@ void Comm(void *param)
 	_endthread();
 }
 
-bool cLoginServer::CommLoop()
+bool LoginServer::CommLoop()
 {
 	bool bExit = false;
 	int retbufsize = 0;
@@ -124,18 +124,13 @@ bool cLoginServer::CommLoop()
 		}
 		else
 			Log::Out("Accept Client: {}", inet_ntoa(client.sin_addr));
-		int id = -1;
-		socketWithUID *arg;
-		arg = (socketWithUID *)malloc(sizeof(socketWithUID));
-		arg->msg_socket = (void*)msg_socket;
-		arg->usr_id = id;
-		_beginthread(Comm, 0, (void*)arg);
+		_beginthread(Comm, 0, (void*)msg_socket);
 	}
 
 	return true;
 }
 
-void cLoginServer::outBuffer()
+void LoginServer::outBuffer()
 {
 	/*
 	printf("---- Recieved Data From %s  ----\n",inet_ntoa(client.sin_addr));

Login32/LoginServer.h
@@ -5,10 +5,10 @@
 
 #include "PacketHandler.h"
 
-class cLoginServer {
+class LoginServer {
 public:
-	cLoginServer();
-	~cLoginServer();
+	LoginServer();
+	~LoginServer();
 	bool Start();
 	bool CommLoop();
 	void outBuffer();
@@ -21,9 +21,5 @@ private:
 	SOCKET listen_socket, msg_socket;
 };
 
-typedef struct {
-	void* msg_socket;
-	int usr_id;
-}socketWithUID;
 
 #endif

Login32/main.cpp
@@ -7,15 +7,11 @@ int main()
 {
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 	//printf("%x\n",sizeof(TrainingDoneResponse));
-	cLoginServer *LoginServer = new cLoginServer;
+	UniquePtr<LoginServer> login_server = MakeUnique<LoginServer>();
 
-	if (LoginServer->Start())
+	if (login_server->Start())
 		printf("----- Server Started -----\n");
 
-	LoginServer->CommLoop();
-	delete LoginServer;
-
-	printf("Server Closing\n");
-
+	login_server->CommLoop();
 	return 0;
 }